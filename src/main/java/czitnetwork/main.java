b91d9ae35baff09175cdf7f99cbdf0abfd8d9782
0Ndyss <suskaondrej@seznam.cz>
2024-02-29 15:51:59 +0100

    first sout

---
+++ src/main/java/czitnetwork/main.java
@@ -0,0 +1,8 @@
+package czitnetwork;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("No tak zdravim, zdravim");
+        System.out.println("Dobré dopo :-)");
+    }
+}
