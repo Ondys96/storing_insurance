a7d36fc430b12e0dd23e44a6f515a4a9d36bcd3e
0Ndyss <suskaondrej@seznam.cz>
2024-02-29 16:01:22 +0100

    importing class

--- src/main/java/czitnetwork/main.java
+++ src/main/java/czitnetwork/main.java
@@ -1,5 +1,8 @@
 package czitnetwork;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("No tak zdravim, zdravim");
